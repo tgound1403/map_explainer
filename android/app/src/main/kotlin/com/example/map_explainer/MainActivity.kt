41f5f7b42a098c3b63447a0276106199d8092e5f
tgound1403 <nguyentrieuduong14032002@gmail.com>
2024-09-14 00:39:28 +0700

    update for native part

---
+++ android/app/src/main/kotlin/com/example/map_explainer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.map_explainer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,7 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyBF053k0P5ARcxG5c6ZmBNa6PpnzSdU62k")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
